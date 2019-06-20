1e206cb612ecb13914959d6dc78b9af8acc93e28
ilabstaff <ilabstaff@172.16.116.71>
2019-06-19 19:25:14 -0700

    robot square

--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -12,22 +12,30 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot king=new Robot();
 
         // 3. Put the robot's pen down
+king.penDown();
 
+king.setSpeed(100);
 
         // 6. Make the robot move as fast as possible
-
+for (int j = 0; j < 10; j++) {
+	
 
         // 5. Do everything below here 4 times
+for (int i = 0; i < 7; i++) {
+	
 
 
         //         2. Move your robot 200 pixels
-
+king.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
+king.turn(360/7);
 
     }
+king.setRandomPenColor();
+king.move(25);
 }
+}}
---
+++ src/_02_robot_graffiti/king.java
@@ -0,0 +1,56 @@
+package _02_robot_graffiti;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class king {
+public static void main(String[] args) {
+	Robot king=new Robot();
+king.penDown();
+king.setRandomPenColor();
+king.move(100);
+king.turn(90);
+king.move(100);
+king.turn(90);
+king.move(100);
+king.turn(90);
+king.move(100);
+king.move(100);
+king.turn(90);
+king.move(100);
+king.turn(90);
+king.move(100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
